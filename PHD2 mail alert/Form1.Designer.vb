ee70a45383544d254874b1cd2eeb94ee0b7e882a
Gangreo <gangreo@gmail.com>
2022-05-13 11:14:41 +0200

    Add project files.

---
+++ PHD2 mail alert/Form1.Designer.vb
@@ -0,0 +1,403 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Location = New System.Drawing.Point(1, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(320, 234)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Label13)
+        Me.TabPage1.Controls.Add(Me.Label12)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.TextBox3)
+        Me.TabPage1.Controls.Add(Me.TextBox2)
+        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(312, 208)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Message"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(52, 184)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(24, 13)
+        Me.Label13.TabIndex = 6
+        Me.Label13.Text = "Idle"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(6, 184)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(40, 13)
+        Me.Label12.TabIndex = 5
+        Me.Label12.Text = "Status:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Subject"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "E-mail"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.PHD2_mail_alert.My.MySettings.Default, "messageBody", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.TextBox3.Location = New System.Drawing.Point(8, 70)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(295, 100)
+        Me.TextBox3.TabIndex = 2
+        Me.TextBox3.Text = Global.PHD2_mail_alert.My.MySettings.Default.messageBody
+        '
+        'TextBox2
+        '
+        Me.TextBox2.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.PHD2_mail_alert.My.MySettings.Default, "subject", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.TextBox2.Location = New System.Drawing.Point(56, 37)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(247, 20)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Text = Global.PHD2_mail_alert.My.MySettings.Default.subject
+        '
+        'TextBox1
+        '
+        Me.TextBox1.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.PHD2_mail_alert.My.MySettings.Default, "email", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.TextBox1.Location = New System.Drawing.Point(56, 10)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(247, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = Global.PHD2_mail_alert.My.MySettings.Default.email
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(193, 179)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(110, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Watch star"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.Button2)
+        Me.TabPage2.Controls.Add(Me.ComboBox1)
+        Me.TabPage2.Controls.Add(Me.TextBox7)
+        Me.TabPage2.Controls.Add(Me.TextBox6)
+        Me.TabPage2.Controls.Add(Me.Label7)
+        Me.TabPage2.Controls.Add(Me.Label6)
+        Me.TabPage2.Controls.Add(Me.Label5)
+        Me.TabPage2.Controls.Add(Me.Label4)
+        Me.TabPage2.Controls.Add(Me.Label3)
+        Me.TabPage2.Controls.Add(Me.TextBox5)
+        Me.TabPage2.Controls.Add(Me.TextBox4)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(312, 208)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Settings"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 133)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 23)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Send test email"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.PHD2_mail_alert.My.MySettings.Default, "ssloption", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"SSL/TLS", "None"})
+        Me.ComboBox1.Location = New System.Drawing.Point(84, 106)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 9
+        Me.ComboBox1.Text = Global.PHD2_mail_alert.My.MySettings.Default.ssloption
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(84, 81)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(55, 20)
+        Me.TextBox7.TabIndex = 8
+        Me.TextBox7.Text = "587"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.PHD2_mail_alert.My.MySettings.Default, "SMTPpass", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.TextBox6.Location = New System.Drawing.Point(84, 56)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox6.Size = New System.Drawing.Size(219, 20)
+        Me.TextBox6.TabIndex = 7
+        Me.TextBox6.Text = Global.PHD2_mail_alert.My.MySettings.Default.SMTPpass
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(9, 110)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(45, 13)
+        Me.Label7.TabIndex = 4
+        Me.Label7.Text = "Security"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(9, 84)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(58, 13)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "SMTP port"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 59)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 13)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "SMTP pass"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(8, 33)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 13)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "SMTP user"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(8, 10)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "SMTP server"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.PHD2_mail_alert.My.MySettings.Default, "SMTPuser", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.TextBox5.Location = New System.Drawing.Point(84, 31)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(219, 20)
+        Me.TextBox5.TabIndex = 6
+        Me.TextBox5.Text = Global.PHD2_mail_alert.My.MySettings.Default.SMTPuser
+        '
+        'TextBox4
+        '
+        Me.TextBox4.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.PHD2_mail_alert.My.MySettings.Default, "SMTPserver", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.TextBox4.Location = New System.Drawing.Point(84, 7)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(219, 20)
+        Me.TextBox4.TabIndex = 5
+        Me.TextBox4.Text = Global.PHD2_mail_alert.My.MySettings.Default.SMTPserver
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.Label14)
+        Me.TabPage3.Controls.Add(Me.Label11)
+        Me.TabPage3.Controls.Add(Me.Label10)
+        Me.TabPage3.Controls.Add(Me.Label9)
+        Me.TabPage3.Controls.Add(Me.Label8)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Size = New System.Drawing.Size(312, 208)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "About"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(3, 191)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(63, 13)
+        Me.Label14.TabIndex = 4
+        Me.Label14.Text = "App version"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label11.Location = New System.Drawing.Point(10, 13)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(71, 13)
+        Me.Label11.TabIndex = 3
+        Me.Label11.Text = "Description"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(23, 30)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(197, 26)
+        Me.Label10.TabIndex = 2
+        Me.Label10.Text = "A simple application that sends an email " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "when a star is lost in PHD2 guding"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(26, 87)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(128, 13)
+        Me.Label9.TabIndex = 1
+        Me.Label9.Text = "Jurij Stare (starej@t-2.net)"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label8.Location = New System.Drawing.Point(10, 70)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(68, 13)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "Created by"
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(320, 233)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(336, 271)
+        Me.MinimumSize = New System.Drawing.Size(336, 271)
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "PHD2 mail alert"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.TabPage3.ResumeLayout(False)
+        Me.TabPage3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label14 As Label
+End Class
---
+++ PHD2 mail alert/Form1.vb
@@ -0,0 +1,153 @@
+﻿Imports System.Net.Mail
+Imports System.Net.Sockets
+
+
+Public Class Form1
+    Dim tcpClient As TcpClient
+    Dim lostCounter As Integer = 0
+
+    'prevent altering the dropdown menu
+    Private Sub ComboBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles ComboBox1.KeyPress
+        e.Handled = True
+    End Sub
+
+    'test mail button
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        sendMail()
+    End Sub
+
+    'send mail routine
+    Private Sub sendMail()
+        'check for required fields
+        If TextBox1.Text.Length = 0 Or TextBox4.Text.Length = 0 Or TextBox5.Text.Length = 0 Or TextBox6.Text.Length = 0 Or TextBox7.Text.Length = 0 Then
+            MessageBox.Show("All fields are mandatory (+ your email)!")
+            Return
+        End If
+
+
+
+        Dim Mail As New MailMessage
+        Dim SMTP As New SmtpClient(TextBox4.Text)
+
+        Mail.Subject = TextBox2.Text
+        Mail.From = New MailAddress(TextBox1.Text)
+        SMTP.Credentials = New System.Net.NetworkCredential(TextBox5.Text, TextBox6.Text)
+
+        Mail.To.Add(TextBox1.Text)
+        Mail.IsBodyHtml = True
+        Mail.Body = TextBox3.Text
+
+        'enable ssl option
+        If ComboBox1.SelectedItem = "SSL/TLS" Then
+            SMTP.EnableSsl = True
+        Else
+            SMTP.EnableSsl = False
+        End If
+
+        SMTP.Port = TextBox7.Text
+
+        Try
+            SMTP.Send(Mail)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    'enable watching
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'reset lost counter
+        lostCounter = 0
+
+        'check for required fields
+        If TextBox1.Text.Length = 0 Or TextBox4.Text.Length = 0 Or TextBox5.Text.Length = 0 Or TextBox6.Text.Length = 0 Or TextBox7.Text.Length = 0 Then
+            MessageBox.Show("Check entered email fields!")
+            Return
+        End If
+
+        If Button1.Text = "Watch star" Then
+            tcpClient = New TcpClient
+
+            Try
+                'default PHD2 server address and port
+                tcpClient.Connect("127.0.0.1", 4400)
+            Catch ex As Exception
+                MessageBox.Show("PHD2 not running?")
+                Return
+            End Try
+
+
+            'watch interval
+            Timer1.Interval = 5000
+
+            Timer1.Start()
+
+
+            Button1.Text = "Stop watching"
+            Label13.Text = "Waiting..."
+        Else
+            'close connection and stop timer
+            tcpClient.Close()
+            Timer1.Stop()
+            Button1.Text = "Watch star"
+            Label13.Text = "Idle"
+        End If
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim networkStream As NetworkStream = tcpClient.GetStream()
+        If networkStream.CanWrite And networkStream.CanRead Then
+
+            ' Send appstate request
+            Dim sendBytes As [Byte]() = System.Text.Encoding.ASCII.GetBytes("{""method"":""get_app_state"", ""id"": 1}" & vbCrLf)
+            networkStream.Write(sendBytes, 0, sendBytes.Length)
+
+            ' Read response
+            Dim bytes(tcpClient.ReceiveBufferSize) As Byte
+            Try
+                networkStream.Read(bytes, 0, CInt(tcpClient.ReceiveBufferSize))
+            Catch ex As Exception
+                MessageBox.Show("PHD2 not running?")
+                Button1_Click(Nothing, Nothing)
+                Return
+            End Try
+
+
+            Dim response As String = System.Text.Encoding.ASCII.GetString(bytes)
+
+            'check message if they contain contains...
+            If response.IndexOf("StarLost") > -1 Then
+                lostCounter += 1
+                Label13.Text = "Star lost!!!"
+
+                If lostCounter = 5 Then
+                    lostCounter = 0
+                    'stop watching
+                    Button1_Click(Nothing, Nothing)
+
+                    'send mail
+                    sendMail()
+                End If
+
+
+            ElseIf response.IndexOf("GuideStep") > -1 Then
+                Label13.Text = "Guding"
+                lostCounter = 0
+            Else
+                lostCounter = 0
+                Label13.Text = "Doing something else..."
+            End If
+
+
+        End If
+    End Sub
+
+    Private Sub TabControl1_Leave(sender As Object, e As EventArgs) Handles TabControl1.Leave
+        My.Settings.Save()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label14.Text = "App version " & Application.ProductVersion
+    End Sub
+End Class
